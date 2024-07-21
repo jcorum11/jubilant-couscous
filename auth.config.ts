b37e8fcd5f6f9bbffa72db32407b57395638d8df
Jacob Corum <jacob.w.corum@gmail.com>
2024-07-20 23:25:15 -0400

    chapter 14

---
+++ auth.config.ts
@@ -0,0 +1,26 @@
+import type { NextAuthConfig } from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+import { z } from "zod";
+import { sql } from "@vercel/postgres";
+import type { User } from "@/app/lib/definitions";
+import bcrypt from "bcrypt";
+
+export const authConfig = {
+  pages: {
+    signIn: "/login",
+  },
+  callbacks: {
+    authorized({ auth, request: { nextUrl } }) {
+      const isLoggedIn = !!auth?.user;
+      const isOnDashboard = nextUrl.pathname.startsWith("/dashboard");
+      if (isOnDashboard) {
+        if (isLoggedIn) return true;
+        return false; // Redirect unauthenticated users to login page
+      } else if (isLoggedIn) {
+        return Response.redirect(new URL("/dashboard", nextUrl));
+      }
+      return true;
+    },
+  },
+  providers: [],
+} satisfies NextAuthConfig;
